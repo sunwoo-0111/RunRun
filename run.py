5accdee43de6a76e82c966f59eac3a5e4158313a
sunwoo-0111 <mario0111@naver.com>
2024-12-05 16:09:06 +0900

    최종 코드

--- run.py
+++ run.py
@@ -1,14 +1,43 @@
 import time
-import pygame as pg
-from PIL import Image, ImageDraw, ImageFont
 import random
+from colorsys import hsv_to_rgb
+import board
+from gpiozero import Button
+from digitalio import DigitalInOut
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_rgb_display import st7789
 
-from pygame.examples.aliens import Player
+# 디스플레이 생성
+cs_pin = DigitalInOut(board.CE0)
+dc_pin = DigitalInOut(board.D25)
+reset_pin = DigitalInOut(board.D24)
+BAUDRATE = 24000000  # SPI 통신 속도 조절
 
-# 파이게임 초기화 240x240
-pg.init()
-screen = pg.display.set_mode((240, 240))
-clock = pg.time.Clock()
+spi = board.SPI()
+disp = st7789.ST7789(
+    spi,
+    height=240,
+    y_offset=80,
+    rotation=180,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+
+# 입력 핀:
+button_A = Button(5)
+button_B = Button(6)
+button_L = Button(27)
+button_R = Button(23)
+button_U = Button(17)
+button_D = Button(22)
+button_C = Button(4)
+
+# 백라이트 켜기
+backlight = DigitalInOut(board.D26)
+backlight.switch_to_output()
+backlight.value = True
 
 #최고점수 로딩
 try:
@@ -19,12 +48,12 @@ except:
 
 # 시작화면
 def start_game():
-    start_image = Image.open("/Users/limsunwoo/runrun/images/Start.png").convert("RGB")
+    start_image = Image.open("images/Start.png").convert("RGB")
     draw = ImageDraw.Draw(start_image)
     
     print(start_image.size)
     print(start_image.mode)
-    font_path = "/Users/limsunwoo/runrun/font/continuous.ttf"
+    font_path = "font/continuous.ttf"
     font_large = ImageFont.truetype(font_path, size=20)
     font_small = ImageFont.truetype(font_path, size=12)
 
@@ -33,18 +62,11 @@ def start_game():
 
     running = True
     while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                exit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_a:  # A 키를 누르면 시작
-                    running = False
+        if button_A.is_pressed:
+            running = False
 
-        # 이미지를 PyGame 화면에 출력
-        screen.blit(pg.image.fromstring(start_image.tobytes(), start_image.size, "RGB"), (0, 0))
-        pg.display.flip()
-        clock.tick(15)
+        # 이미지를 화면에 출력
+        disp.image(start_image)
 
 
 restart = True
@@ -54,7 +76,7 @@ def Game():
     global restart, high_score
     restart = False
     
-    backgroundImage = Image.open("/Users/limsunwoo/runrun/images/background.png").convert("RGB")
+    backgroundImage = Image.open("images/background.png").convert("RGB")
 
     block = []
     for i in range(20):
@@ -91,11 +113,11 @@ def Game():
     playerPos = [12 * 10, 10 * 12]
     playerSize = [12, 24]
 
-    playerImage = Image.open("/Users/limsunwoo/runrun/images/Thief1.png").convert("RGBA")
-    drawPlayerImage = ImageDraw.Draw(playerImage)
+    playerImage = Image.open("images/Thief1.png").convert("RGBA")
+    playerImage_ = Image.open("images/Thief1_.png").convert("RGBA")
 
-    playerSlideImage = Image.open("/Users/limsunwoo/runrun/images/sliding.png")
-    drawPlayerImage = ImageDraw.Draw(playerSlideImage)
+    playerSlideImage = Image.open("images/sliding.png")
+    playerSlideImage_ = Image.open("images/sliding_.png")
 
     # 추격자 구현
     chaserPos = [-24 ,playerPos[1]]
@@ -108,8 +130,7 @@ def Game():
     playerSlide = False
 
     # 추격자 그리기
-    chaserImage = Image.open("/Users/limsunwoo/runrun/images/chaser.png")
-    drawchaserImage = ImageDraw.Draw(chaserImage)
+    chaserImage = Image.open("images/chaser.png")
 
     # 목숨 아이템 구현
     heartPos = None
@@ -138,22 +159,23 @@ def Game():
     # 게임 루프
     running = True
     paused = False
+    font_path = "font/continuous.ttf"
+    font_12 = ImageFont.truetype(font_path, size = 12)
+    font_20 = ImageFont.truetype(font_path, size = 20)
+    
+    def paused_game():
+        nonlocal paused
+        paused = not paused
     while running:
         
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_b:
-                    paused = not paused
+        button_C.when_pressed = paused_game
    
-        keys = pg.key.get_pressed()
-        if keys[pg.K_UP]:
+        if button_U.is_pressed:
             if jumpable:
                 playerVerticalSpeed -= 8 
                 jumpable = False
 
-        if keys[pg.K_DOWN]:
+        if button_D.is_pressed:
             playerSlide = True
         else:
             if playerSlide:
@@ -164,21 +186,41 @@ def Game():
             newMap(block, isblock, holeCount)
         score += 1
 
+
         playerPos[1] += playerVerticalSpeed
         playerVerticalSpeed += 1.2
         playerVerticalSpeed = min(playerVerticalSpeed, 5)
 
-        usePlayerImage = playerImage.copy()
-        if playerSlide:
-            playerSize = [24, 12]
-            usePlayerImage=playerSlideImage
+        if time.time() < invincible_until:
+            invincible = True # 무적 상태 활성화 플래그
         else:
-            playerSize = [12,24]
+            invincible = False # 다시 끄기
+
+        if invincible:
+            usePlayerImage = playerImage_.copy()
+            if playerSlide:
+                playerSize = [24, 12]
+                usePlayerImage = playerSlideImage_.copy()
+            else:
+                playerSize = [12, 24]
+
+        else:
+            usePlayerImage = playerImage.copy()
+            if playerSlide:
+                playerSize = [24, 12]
+                usePlayerImage = playerSlideImage.copy()
+            else:
+                playerSize = [12, 24]
 
         jumpable = False
         # 충돌했을 때 벽에 밀려 위치가 애매해지는 현상 해결
         # 충돌 체크
         for b in block:
+            if b[0] > 140:
+                continue
+            if b[0] < -20:
+                block.remove(b)
+                continue
             col = collision(playerPos,playerSize, b)
             if col == 1:
                 playerPos[1] = b[1] - playerSize[1]
@@ -195,12 +237,6 @@ def Game():
                         running = False
                 playerPos[0] = b[0] - playerSize[0]
 
-
-        if time.time() < invincible_until:
-            usePlayerImage = usePlayerImage.point(lambda p:p//2)
-            invincible = True # 무적 상태 활성화 플래그
-        else:
-            invincible = False # 다시 끄기
 
         #recovering
         if playerPos[0] < 120:
@@ -269,30 +305,30 @@ def Game():
             draw.rounded_rectangle((
                 b[0], b[1], b[0] + 12, b[1] + 12
             ), 2, (0, 0, 0))
-            if(score < 300):
+            if(score < 200):
                 b[0] -= 3
-            elif(score < 600):
+            elif(score < 400):
                 b[0] -= 3.5
-            elif(score < 1000):
+            elif(score < 600):
                 b[0] -= 4
             else:
                 b[0] -= 6
 
         # running 중 현재점수, 남은 목숨 , 최고점수 띄우기
-        font = ImageFont.load_default()
+        
         if(score <= high_score):
-            draw.text((5,5), f"Score: {score}", fill=(0,0,0), font=font)
+            draw.text((5,5), f"Score: {score}", fill=(0,0,0), font=font_12)
         else:
-            draw.text((5,5), f"Score: {score}", fill=(255,0,0), font=font)
-        draw.text((200,5), f"Lives: {lives}", fill=(255,0,0), font=font)
-        draw.text((100,5),f"high: {high_score}", fill=(0,0,0),font=font)
+            draw.text((5,5), f"Score: {score}", fill=(255,0,0), font=font_12)
+        draw.text((200,5), f"Lives: {lives}", fill=(255,0,0), font=font_12)
+        draw.text((100,5),f"high: {high_score}", fill=(0,0,0),font=font_12)
 
         # 플레이어 이미지 그리기
         bg.paste(usePlayerImage, (int(playerPos[0]), int(playerPos[1])))
         bg.paste(chaserImage, (int(chaserPos[0]),int(chaserPos[1])))
         # 하트 아이템 구현
 
-        heartImage = Image.open("/Users/limsunwoo/runrun/images/heart.png")
+        heartImage = Image.open("images/heart.png")
         if heartPos is None and lives<3 and random.randint(0,100) < 1:
             heartPos = [240,240-36]
         
@@ -310,9 +346,8 @@ def Game():
         if playerPos[1] > 240:
             running = False
 
-        screen.blit(pg.image.fromstring(bg.tobytes(), bg.size, "RGB"), (0, 0))
-        pg.display.flip()
-        clock.tick(15)
+        disp.image(bg)
+        time.sleep(0.01)
 
     bg = backgroundImage.copy()
     draw = ImageDraw.Draw(bg)
@@ -327,14 +362,9 @@ def Game():
     # 게임 오버 화면
     isScoreDraw = False
     while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                exit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_a:
-                    restart = True
-                    return
+        if button_A.is_pressed:
+            restart = True
+            break
 
         bg_ = bg.copy()
         draw = ImageDraw.Draw(bg_)
@@ -342,10 +372,8 @@ def Game():
         # 결과 점수 반환 및 재시작 여부
         
         if isScoreDraw:
-            font_path = "/Users/limsunwoo/runrun/font/continuous.ttf"
-            font = ImageFont.truetype(font_path, size = 20)
-            draw.text((110,90), str(score), fill=(0, 0, 0), font=font)
-            draw.text((45,140), f"Press A to restart", fill=(0,0,0), font=font)
+            draw.text((110,90), str(score), fill=(0, 0, 0), font=font_20)
+            draw.text((45,140), f"Press A to restart", fill=(0,0,0), font=font_20)
             
             if score > high_score:
                 high_score = score
@@ -355,9 +383,8 @@ def Game():
             
         isScoreDraw = not isScoreDraw
 
-        screen.blit(pg.image.fromstring(bg_.tobytes(), bg_.size, "RGB"), (0, 0))
-        pg.display.flip()
-        time.sleep(0.5)
+        disp.image(bg_)
+        time.sleep(0.1)
 
 while(True):
     start_game()
